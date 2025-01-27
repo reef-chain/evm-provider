3d731e3bba3a6e2742dfc2e8d8c9f55a7bc4ef3f
matjazonline <matjazonline@gmail.com>
2025-01-27 12:28:38 +0100

    export utils

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './SigningKey';
 export * from './TestAccountSigningKey';
 export { U64MAX as MAX_GAS_LIMIT } from './utils';
 export { U32MAX as MAX_STORAGE_LIMIT } from './utils';
+export * from './utils'
